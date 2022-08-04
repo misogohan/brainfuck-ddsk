2f2885f3e864c5f1d353b97b43146de8f25c7783
misogohan <soypasterice@gmail.com>
2022-08-04 13:31:22 +0900

    add notice

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,24 @@ import { run as bfRun } from './brainfuck/mod';
 import './App.css'
 
 const randomGen = function *() { for (;;) yield (Math.random() * 2 | 0) + 48; }();
+const initialCode = `This is a Brainfuck runtime
+Here is hello world
++++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+++.>-.
+------------.<++++++++.--------.+++.------.--------.>+.
+
+>++++++++++..>
+
+You can get random characters of '0' or '1'
+++++++++[>++++++++<-]>[<,.>-]
+
+In Brainfuck write DDSK program (https://twitter.com/Sheeeeepla/status/1554028833942441984) here
+
+shortcut:
+  Ctrl+Shift+Enter: run the code
+`
 
 function App() {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState(initialCode);
   const [output, setOutput] = useState('');
 
   const onCodeChange = (ev: ChangeEvent<HTMLTextAreaElement>) => {
@@ -20,10 +35,10 @@ function App() {
   return (
     <div className="App">
       <h1>DDSK in Brainfuck!</h1>
-      <textarea className="code" value={code} onChange={onCodeChange} onKeyDown={cse}/>
+      <textarea spellCheck="false" className="code" value={code} onChange={onCodeChange} onKeyDown={cse}/>
       <div className="output">
         <nav><button className="run" onClick={run}>Run</button></nav>
-        <code>{ output }</code>
+        <pre>{ output }</pre>
       </div>
     </div>
   )
